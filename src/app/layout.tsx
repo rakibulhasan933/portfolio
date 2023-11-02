3e45ab4be001951006601d68fe110e503f265e1c
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-03 01:23:11 +0600

    [ADD] navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import StarsCanvas from '@/components/StarBackground'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
+        <Navbar />
         <StarsCanvas />
         {children}
       </body>
