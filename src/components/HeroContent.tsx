e084f9b112add5a5be7b44f02118558f2f4c8454
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-02 23:59:20 +0600

    [ADD] title added

--- src/components/HeroContent.tsx
+++ src/components/HeroContent.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React from 'react'
 import { motion } from 'framer-motion'
 import { slideInFromTop } from '@/utlis/motion';
+import { SparklesIcon } from '@heroicons/react/20/solid';
 
 function HeroContent() {
 	return (
@@ -14,7 +16,8 @@ function HeroContent() {
 					variants={slideInFromTop}
 					className='Welcome-box py-[15px] px-[4px] border boder-[#7042f88b] opacity-[0.9]'
 				>
-
+					<SparklesIcon className=' text-[#b49bff] mr-[10px] h-5 w-5' />
+					<h1 className="Welcome-text text-[13px]">Full Stack Developer</h1>
 				</motion.div>
 
 			</div>
