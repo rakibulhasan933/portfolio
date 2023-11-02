cd83fb620dde74cd5f3acdde940281581262c1e1
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-02 23:33:24 +0600

    [ADD] photos added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className='h-full w-full'>
-      <div className="flex flex-col gap-20">
-        <h1 className=" text-white">Rakibul Portfolio</h1>
+      <div className="flex flex-col h-[850px] gap-20">
+        <Hero />
       </div>
     </main>
   )
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Hero() {
 	return (
-		<div>Hero</div>
+		<div className=' relative flex flex-col h-full w-full'>
+			<video
+				autoPlay
+				muted
+				loop
+				className=' rotate-180 absolute top-[-340px] h-full w-full left-0 z-[1] object-cover'
+			>
+				<source src='/blackhole.webm' type='video/webm' />
+			</video>
+		</div>
 	)
 }
 
