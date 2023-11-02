4ff4280741e8a2b02620a9b382965ed90a3cbc23
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-03 01:00:59 +0600

    [ADD] background star

--- src/components/HeroContent.tsx
+++ src/components/HeroContent.tsx
@@ -15,19 +15,16 @@ function HeroContent() {
 			<div className=' h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
 				<motion.div
 					variants={slideInFromTop}
-					className='Welcome-box py-[8px] px-[4px] border boder-[#7042f88b] opacity-[0.9]'
+					className='Welcome-box py-[7px] px-[4px] border boder-[#7042f88b] opacity-[0.9]'
 				>
 					<SparklesIcon className=' text-[#b49bff] mr-[10px] h-5 w-5' />
 					<h1 className="Welcome-text text-[13px]">Full Stack Developer</h1>
 				</motion.div>
 				<motion.div
 					variants={slideInFromLeft(0.5)}
-					className=' flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'
+					className=' flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'
 				>
-					Providing
-					<span className=' text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>the best</span>
-					Project experiences
-
+					<span>Providing<span className=' text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>the best</span>Project experiences</span>
 				</motion.div>
 				<motion.div
 					variants={slideInFromLeft(0.8)}
