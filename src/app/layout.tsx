c45ce09190afcd57c0307375972ad0ecb90041e0
rakibulhasan933 <rakibulmd933@gmail.com>
2023-11-03 12:49:21 +0600

    [ADD] footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import StarsCanvas from '@/components/StarBackground'
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,9 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
-        <Navbar />
         <StarsCanvas />
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   )
