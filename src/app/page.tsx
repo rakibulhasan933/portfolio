9814fbbd2adfd7050a921e60f27d1dcbb90b0a46
rakibulhasan933 <rakibulmd933@gmail.com>
2023-12-22 01:47:08 +0600

    [ADD] products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
   return (
     <main>
-      <section className="h-[100px]">
+      {/* <section className="h-[100px]">
         <Header />
       </section>
       <section className="h-screen">About</section>
       <section className="h-screen">Parallax</section>
       <section className="h-screen">services</section>
       <section className="h-screen">Portfolio</section>
-      <section className="h-screen">contact</section>
+      <section className="h-screen">contact</section> */}
+      <Sidebar />
     </main>
   )
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+	return (
+		<div className='flex items-center justify-center h-screen'>
+			<div className="bg-white w-[200px] h-[200px]"></div>
+		</div>
+	)
+}
+
+export default Sidebar
