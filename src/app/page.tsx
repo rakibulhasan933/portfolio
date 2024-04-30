a47c55f7cbd0cbb0f4b3d3ed3d7648b7b5b16d69
rakibulhasan933 <rakibulmd933@gmail.com>
2024-04-30 15:42:59 +0600

    [ADD] final setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,8 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="">
-        🚀 Welcome to Rakibul Hasan&apos;s Creative Haven! <br />
-
-        <p className="py-2"> Dear Incredible Visitors,</p><br />
-
-        <p className=" py-4">
-          Step into the vibrant world of Rakibul Hasan, where pixels dance, ideas collide, and creativity knows no bounds. It&apos;s not just a portfolio; it&apos;s a curated showcase of my artistic odyssey, and I&apos;m thrilled to have you along for the journey.
-        </p> <br />
-
-        🎨 Why Here? <br />
-        <p className="py-4">
-          This space is more than a collection of projects; it&apos;s a gallery of dreams brought to life. From the initial spark of inspiration to the final brushstroke of code, each creation is a chapter in my creative narrative. Get ready to immerse yourself in a symphony of colors and innovation.</p> <br />
-
-        🌌 Explore the Galaxies of Creativity <br />
-        <p className="py-4">
-          Navigate through constellations of projects, each telling a unique story. Each click is an invitation to delve into the depths of my passion. Your curiosity is the compass feel free to roam, click, and uncover the wonders within.</p> <br />
-
-        🚀 Embark on an Odyssey of Innovation <br />
-        <p className="py-4">In this digital odyssey, innovation is the guiding star. Expect sleek designs, cutting-edge solutions, and a commitment to pushing the boundaries. Together, let&apos;s turn ideas into reality and redefine what&apos;s possible. <br /></p>
-
-        📬 Connect and Collaborate <br />
-        <p className="py-4">This isn`&apos;`t just about me; it&apos;s about us. Whether you&apos;re an explorer, a potential collaborator, or simply someone who appreciates the beauty of the digital canvas, I invite you to reach out. Let`&apos;`s build something extraordinary together! </p> <br />
-
-        🌟 Thank You for Being Here <br />
-        <p className="py-4"> Your presence makes this space come alive, and for that, I&apos;m truly grateful. Thank you for taking a moment to explore, appreciate, and perhaps find inspiration. <br /></p>
-
-        <p className="py-4">Ready to embark on this creative journey together?</p> <br />
-
-        <p className="py-4">Cheers to pixels, passion, and endless possibilities!</p> <br />
-
-        Rakibul Hasan
-      </div>
+    <main>
+      <h2 className="">Hello Word</h2>
     </main>
   );
 }
