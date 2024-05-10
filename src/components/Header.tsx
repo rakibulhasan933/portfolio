0372b409106ba877f6fd2ec483edca2857983bfa
rakibulhasan933 <rakibulmd933@gmail.com>
2024-05-11 01:51:19 +0600

    [ADD] animation header text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
 import Link from 'next/link'
+import { TextGenerateEffect } from './ui/text-generate-effect'
 
 function Header() {
 	return (
@@ -8,8 +9,10 @@ function Header() {
 			<div className='flex flex-col md:gap-y-4 gap-y-2'>
 				<div className="flex flex-col md:leading-[78px] leading-relaxed gap-0">
 					<p className=" text-emerald-400 font-mono md:text-[20px] text-[16px] font-normal">Hi, my name is</p>
-					<h1 className="text-[#CCD6F6] md:text-[80px] text-[31px] font-bold">MD Rakibul Hasan.</h1>
-					<h2 className="text-[#8892B0] md:text-[80px] text-[31px] font-bold">I build things for the web.</h2>
+					{/* <h1 className="text-[#CCD6F6] md:text-[80px] text-[31px] font-bold">MD Rakibul Hasan.</h1> */}
+					<TextGenerateEffect className="text-[#CCD6F6] md:text-[80px] text-[31px] font-bold" words='MD Rakibul Hasan.' />
+					{/* <h2 className="text-[#8892B0] md:text-[80px] text-[31px] font-bold">I build things for the web.</h2> */}
+					<TextGenerateEffect words='I build things for the web.' className="text-[#8892B0] md:text-[80px] text-[31px] font-bold" />
 				</div>
 				<p className=" text-[#B2BEDE] font-normal text-[18px]  mb-4">I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused  on building accessible, human-centered products. </p>
 				<Button variant="outline" asChild className=' bg-inherit hover:bg-emerald-400 border-2 hover:text-white hover:border-emerald-400 border-emerald-400  text-emerald-400  w-[120px] px-4 pt-3 text-[18px] hover:shadow-[4px_4px_0px_0px_rgba(255,255,255)] transition duration-200'>
