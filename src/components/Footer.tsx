650cba3d7aa7f1a508e6084b0df0e68b90b8b023
rakibulhasan933 <rakibulmd933@gmail.com>
2024-05-13 18:43:36 +0600

    [ADD] footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,18 @@
 import { useGSAP } from '@gsap/react';
 import React from 'react'
 import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 
 function Footer() {
 	useGSAP(() => {
-		gsap.fromTo("#footer", { y: -300, scale: 2, opacity: 1, duration: 1 }, { y: 0, scale: 1, opacity: 1, duration: 1 });
-	})
+		gsap.registerPlugin(ScrollTrigger);
+		gsap.from("#footer", {
+			scrollTrigger: {
+				trigger: "#footer",
+				toggleActions: "restart none none none",
+			}, y: -300, scale: 2, opacity: 1, duration: 1.5
+		});
+	});
 	return (
 		<p id='footer' className="text-center py-2 text-gray-300 text-[15px]  hover:text-emerald-400">
 			{new Date().getFullYear()} &copy;  Designed & Built by Rakibul Hasan
