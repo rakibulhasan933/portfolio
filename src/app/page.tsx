95c51d9c8d87f8360e1e1afd94191cae11a47e5e
rakibulhasan933 <rakibulmd933@gmail.com>
2024-08-16 22:23:58 +0600

    [ADD] background cover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Project from "@/components/Project";
 import ShortProject from "@/components/ShortProject";
+import { Boxes } from "@/components/ui/background-boxes";
 
 export default function Home() {
   return (
     <main className="md:py-4 py-2">
+      <Boxes />
       <Navbar />
       <div className="my-[30px] px-4  md:px-[150px]">
         <Header />
