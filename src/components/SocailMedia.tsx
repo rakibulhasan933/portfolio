334b1d639de9d34d04dde46c5eb5622c142e4c64
rakibulhasan933 <rakibulmd933@gmail.com>
2024-08-18 12:58:38 +0600

    [ADD] facebook card

--- src/components/SocailMedia.tsx
+++ src/components/SocailMedia.tsx
@@ -1,5 +1,4 @@
-import { EnvelopeOpenIcon } from '@radix-ui/react-icons';
-import { Facebook, FacebookIcon, Linkedin, Mail, PhoneCall, Twitter } from 'lucide-react';
+import { Facebook, Mail, Twitter } from 'lucide-react';
 import React from 'react';
 import Link from 'next/link';
 
@@ -7,11 +6,11 @@ const SocialMedia = () => {
     return (
         <div className='fixed mt-[40vh] cursor-pointer md:block'>
             <div className='relative text-white '>
-                <Link href='https://www.linkedin.com/in/rakibulhossainrakib/'
+                <Link href='https://www.facebook.com/mdrakibulhasan.rakib6'
                     className="flex flex-row gap-2 px-4 items-center h-12 bg-gray-500/5 rounded-lg hover:bg-gray-500/10 transition-all duration-200 text-zinc-200  hover:text-white absolute top-[36px] -left-24 hover:left-0 group z-50"
                 >
                     <p className='w-full mr-10 text-white text-sm whitespace-nowrap'>Contact me</p>
-                    <Linkedin className='text-xl ml-24 absolute h-12 transition-all duration-200 text-zinc-400 hover:text-blue-500 group-hover:text-white '></Linkedin>
+                    <Facebook className='text-xl ml-24 absolute h-12 transition-all duration-200 text-zinc-400 hover:text-blue-500 group-hover:text-white ' />
 
                 </Link>
                 <Link href='https://twitter.com/rakibul_hasan80'
