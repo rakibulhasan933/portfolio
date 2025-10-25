b02045943e6c798d9f941585829dc3760a38c1a2
rakibulhasan933 <rakibulmd933@gmail.com>
2025-10-26 02:07:54 +0600

    updated

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'rakibul-portfolio-v1';
+const urlsToCache = [
+    '/',
+    '/manifest.json',
+    '/_next/static/css/',
+    '/_next/static/js/',
+    '/favicon.ico',
+    '/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Return cached version or fetch from network
+                return response || fetch(event.request);
+            })
+    );
+});
